e5e13a7346643ca66e6fc152c749dd52338dfc44
cbteeple <cbteeple@g.harvard.edu>
2020-07-09 17:32:16 -0400

    added coding plan

--- pressure_control_run/plot_data.py
+++ pressure_control_run/plot_data.py
@@ -21,20 +21,47 @@ class DataParser:
     def __init__(self, data_path = data_base):
         # set up stuff
         self.data_path = data_path
+        self.echo = []
+        self.time = []
+        self.setpoint = []
+        self.measured = []
+        self.master_pressure = []
 
-    # Read in the trajectory and store it in a list of arrays
-    def load_raw_data(self, filename):
+
+    # Read in the raw data
+    def _load_raw_data(self, filename):
+        # If filename exists, open it
+        with open(filename,'r') as f:
+            line = "_" # get one line of the file
+            self._parse_line(line)
+
+
+    def _parse_line(self, line):
+        # If first character is underscore, store that in the "echo" list
+        # Else - the line must contain data
+            # Split line by tabs
+            # Convert first item from ms to sec and store in "time" list
+            # If second item is 0, store the rest of the line in "setpoint" list
+            # Elif second item is 1, store the rest of the line in "measured" list
+            # Elif second item is 2, store the rest of the line in "master_pressure" list
         pass
-
-    def save_data(self, filename, extension='.pkl'):
-        pass
-
 
     def _parse_one(self, filename):
-        # open the data
-        # parse data
-        # save parsed data
+        # open the data and parse it
+        # save parsed data in a better format
         print(filename)
+
+
+    def save_data(self, filename, extension='.pkl'):
+        out = dict()
+        out['echo'] = self.echo
+        out['time'] = self.time
+        out['setpoint'] = self.setpoint
+        out['measured'] = self.measured
+        out['master_pressure'] = self.master_pressure
+
+        # Save the data in a file
+        pass
 
 
 
