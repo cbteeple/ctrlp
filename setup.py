f8da2f4283122db7f77647b74240d1416ffab6cd
cbteeple <cbteeple@g.harvard.edu>
2022-01-03 16:12:06 -0500

    added setup

--- setup.py
+++ setup.py
@@ -1,15 +1,37 @@
-#! /usr/bin/env python
+from setuptools import setup, find_packages
 
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+setup(
+    name='ctrlp',
+    version='2.1.0',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Top-level control interface for a Ctrl-P pressure control system.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'PyYAML',
+        'sorotraj',
+        'pyserial',
+    ],
+    url='https://github.com/cbteeple/pressure_control_interface',
+    author='Clark Teeple',
+    author_email='cbteeple@gmail.com',
+    classifiers=[
+        # How mature is this project? Common values are
+        'Development Status :: 5 - Production/Stable',
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: System :: Hardware :: Hardware Drivers',
 
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
 
-
-setup_args = generate_distutils_setup(
-    packages=['pressure_control_interface'],
-	package_dir={'': ''}
+        # Specify the Python versions you support here.
+        'Programming Language :: Python :: 3',
+    ]
 )
-
-setup(**setup_args)
