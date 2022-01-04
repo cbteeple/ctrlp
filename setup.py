b637482c89aca29a9ed574bc954ec8d0857e5d50
cbteeple <cbteeple@g.harvard.edu>
2022-01-04 17:25:27 -0500

    moved examples and configs to example folder

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ctrlp',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     license='MIT',
