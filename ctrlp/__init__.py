ab428efb0ebd755fc394eb2480d9436a0d465b89
cbteeple <cbteeple@g.harvard.edu>
2022-01-04 12:42:04 -0500

    version bump

--- ctrlp/__init__.py
+++ ctrlp/__init__.py
@@ -4,6 +4,3 @@ from .config_handler import ConfigHandler
 
 from .comm_handler import CommHandler
 from .comm_handler import DataSaver
-
-from .get_files import *
-from .helper_utils import *
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ctrlp',
-    version='0.1.2',
+    version='1.0.0',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     license='MIT',
