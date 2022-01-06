5dec6aea89747dac6ef9896b6fe1b738b7f45830
cbteeple <cbteeple@g.harvard.edu>
2022-01-06 12:30:24 -0500

    added spike command to new command spec

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ctrlp',
-    version='1.0.4',
+    version='1.0.5',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     license='MIT',
